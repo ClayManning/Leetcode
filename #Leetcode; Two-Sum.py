a9d8aaee6b9b4b8d2f552c08c2225128cfe250af
William Manning <wcm145681@gmail.com>
2021-06-03 18:10:02 -0500

    two-sum changes

--- #Leetcode; Two-Sum.py
+++ #Leetcode; Two-Sum.py
@@ -4,10 +4,12 @@
 
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 # Example:
-# Input: li = [2,7,10,34] target = 9
+# Input: nums = [2,7,10,34] target = 9
 # Output [0,1] as 2+7 = 9
 
-def twoSum(nums, target):
+
+#Note that this solution is O(1) iff you are able to sort the list of numbers.
+def twoSumSorted(nums, target):
     nums.sort()
     #O(1) Space Complexity
     start = 0
@@ -22,6 +24,14 @@ def twoSum(nums, target):
         else:
             print("3")
             start+=1
+#This solution is for if you cannot sort the nums
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        difference = target - nums[i]
+        if difference in nums:
+            if(i != nums.index(difference)): #Have to check if it is using the same number twice by mistake
+                return [i,nums.index(difference)]
+    return []
 
 
 nums = [2, 7, 10, 34]
